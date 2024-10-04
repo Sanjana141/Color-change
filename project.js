422f6ee3e957aeef6c42618f30318cf5657c3ac2
Sanjana <160689416+Sanjana141@users.noreply.github.com>
2024-10-04 18:01:02 +0530

    Add files via upload
    Create four buttons, three buttons should be in three different colors , the last button should be a reset button.
    First button of color = red
    Second button of color = blue
    Third button of color = Green
    Fourth button of color = No color 
    
    Task is to do the while clicking on the first button the background color red and button color default color.Remaining
    all the buttons should follow the same task. If i click the reset button it should be return to the initial stage.

---
+++ project.js
@@ -0,0 +1,19 @@
+function Red(){
+    document.getElementById("body").style.backgroundColor="Red"
+    document.getElementById("red").style.backgroundColor="Red"
+}
+
+function Blue(){
+    document.getElementById("body").style.backgroundColor="Blue"
+    document.getElementById("blue").style.backgroundColor="Blue"
+}
+function Green(){
+    document.getElementById("body").style.backgroundColor="Green"
+    document.getElementById("green").style.backgroundColor="Green"
+}
+function Reset(){
+    document.getElementById("body").style.backgroundColor="White"
+    document.getElementById("red").style.backgroundColor="White"
+    document.getElementById("blue").style.backgroundColor="White"
+    document.getElementById("green").style.backgroundColor="White"
+}
